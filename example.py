0e5b7cf9c3769cfd5ffb3d82f4a536f30725c820
Lindo Khoza <willkhoza@gmail.com>
2020-08-07 17:31:17 +0200

    changing code from py2 to py3

--- example.py
+++ example.py
@@ -3,5 +3,5 @@ url = ('http://newsapi.org/v2/top-headlines?'
        'country=us&'
        'apiKey=17d9dbba945d4b1e8b1ed978c7d41867')
 response = requests.get(url)
-print response.json()
+print(response.json())
 
