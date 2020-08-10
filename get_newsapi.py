e592785ad8c1c1c98a7c8edf703aa175bf68fe77
Lindo Khoza <willkhoza@gmail.com>
2020-08-10 05:23:39 +0200

    adding logging feature

--- get_newsapi.py
+++ get_newsapi.py
@@ -2,22 +2,32 @@ from db import read_sql
 from db import Postgres
 import requests
 import pandas as pd
+import datetime
 
 def get_data():
     url = ('http://newsapi.org/v2/top-headlines?'
        'country=za&'
        'apiKey=17d9dbba945d4b1e8b1ed978c7d41867')
-    response = requests.get(url)
-    print(response.json()["status"], "\nTotal Results:", response.json()['totalResults'])
+    try:
+        response = requests.get(url)
+        feedback = [response.json()["status"], "Total Results:", response.json()['totalResults'], datetime.datetime.now()]
+    except:
+        return 0, None, ["fail", "Total Results:", 0,  datetime.datetime.now()]
     data = pd.DataFrame(response.json()["articles"])
     data['source'] = data['source'].apply(lambda info: info['name'])
-    return data
+    return 1, data, feedback
 
 def upload_data(dat, conn_to):
-    dat.to_sql("articles", con = conn_to, if_exists = "replace", index = "False", schema = "news-api")
+    dat.to_sql("articles", con = conn_to, if_exists = "append", index = False, schema = "news-api")
 
 if __name__ == "__main__":
     psql = Postgres()
     conn = psql.connect()
-    data = get_data()
-    upload_data(data, conn)
+    status, data, log = get_data()
+    if status == 1:
+        upload_data(data, conn)
+    log = pd.DataFrame([log])
+    log.columns = ["code", "status", "total", "datetime"]
+    log.drop(["status", "total"], axis=1, inplace=True)
+    log.to_sql("log", con = conn, if_exists = "append", index = False, schema = "news-api")
+    conn.close()
